442740df7f5cb521861413b6b69e4cf40054df8d
Guillaume <guillaume.farbos33@gmail.com>
2023-03-18 18:34:58 +0100

    upd: add geoloc ask button and stylize index css

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,8 @@
-
-import "../css/base.scss";
-import javascriptLogo from "../images/javascript.svg";
-import viteLogo from "../images/vite.svg";
-import { setupCounter } from "./counter.js";
-
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
-
-setupCounter(document.querySelector("#counter"));
+if ("geolocation" in navigator) {
+  document.getElementById("askButton").addEventListener("click", function () {
+    navigator.geolocation.getCurrentPosition(function (location) {
+      appendLocation(location, "fetched");
+    });
+    watchId = navigator.geolocation.watchPosition(appendLocation);
+  });
+}
