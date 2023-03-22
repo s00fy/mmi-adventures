588199fe2660726b0d59a4b6590bf488f7d81bb1
Guillaume <guillaume.farbos33@gmail.com>
2023-03-22 16:18:41 +0100

    upd: nfc reader test

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 const index = document.getElementById("index");
 const rules = document.getElementById("rules");
+const scanButton = document.getElementById("readTag");
 document.getElementById("askButton").addEventListener("click", function () {
   index.style.display = "none";
   rules.style.display = "flex";
@@ -11,26 +12,47 @@ if ("geolocation" in navigator) {
 
   // watchId = navigator.geolocation.watchPosition(appendLocation);
 }
-async function readTag() {
-  if ("NDEFReader" in window) {
+scanButton.addEventListener("click", async () => {
+  console.log("User clicked scan button");
+
+  try {
     const ndef = new NDEFReader();
-    try {
-      await ndef.scan();
-      ndef.onreading = (event) => {
-        const decoder = new TextDecoder();
-        for (const record of event.message.records) {
-          console.log("Record type:  " + record.recordType);
-          console.log("MIME type:    " + record.mediaType);
-          console.log("=== data ===\n" + decoder.decode(record.data));
-        }
-      };
-    } catch (error) {
-      console.log(error);
-    }
-  } else {
-    console.log("Web NFC is not supported.");
+    await ndef.scan();
+    console.log("> Scan started");
+
+    ndef.addEventListener("readingerror", () => {
+      console.log("Argh! Cannot read data from the NFC tag. Try another one?");
+    });
+
+    ndef.addEventListener("reading", ({ message, serialNumber }) => {
+      console.log(`> Serial Number: ${serialNumber}`);
+      console.log(`> Records: (${message.records.length})`);
+    });
+  } catch (error) {
+    console.log("Argh! " + error);
   }
-}
+});
+
+// async function readTag() {
+//   if ("NDEFReader" in window) {
+//     const ndef = new NDEFReader();
+//     try {
+//       await ndef.scan();
+//       ndef.onreading = (event) => {
+//         const decoder = new TextDecoder();
+//         for (const record of event.message.records) {
+//           console.log("Record type:  " + record.recordType);
+//           console.log("MIME type:    " + record.mediaType);
+//           console.log("=== data ===\n" + decoder.decode(record.data));
+//         }
+//       };
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   } else {
+//     console.log("Web NFC is not supported.");
+//   }
+// }
 
 //Génération des 5 clés à trouver dans le batiment MMI
 
