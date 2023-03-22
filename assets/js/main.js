73f08a875546dd506a89be190a256216dbffb8bb
Guillaume <guillaume.farbos33@gmail.com>
2023-03-22 13:53:56 +0100

    upd: test geoloc reader

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,26 +7,26 @@ if ("geolocation" in navigator) {
   });
 }
 
-async function readTag() {
-  if ("NDEFReader" in window) {
-    const ndef = new NDEFReader();
-    try {
-      await ndef.scan();
-      ndef.onreading = (event) => {
-        const decoder = new TextDecoder();
-        for (const record of event.message.records) {
-          consoleLog("Record type:  " + record.recordType);
-          consoleLog("MIME type:    " + record.mediaType);
-          consoleLog("=== data ===\n" + decoder.decode(record.data));
-        }
-      };
-    } catch (error) {
-      consoleLog(error);
-    }
-  } else {
-    consoleLog("Web NFC is not supported.");
-  }
-}
+// async function readTag() {
+//   if ("NDEFReader" in window) {
+//     const ndef = new NDEFReader();
+//     try {
+//       await ndef.scan();
+//       ndef.onreading = (event) => {
+//         const decoder = new TextDecoder();
+//         for (const record of event.message.records) {
+//           consoleLog("Record type:  " + record.recordType);
+//           consoleLog("MIME type:    " + record.mediaType);
+//           consoleLog("=== data ===\n" + decoder.decode(record.data));
+//         }
+//       };
+//     } catch (error) {
+//       consoleLog(error);
+//     }
+//   } else {
+//     consoleLog("Web NFC is not supported.");
+//   }
+// }
 
 // for (let pasMMI = 0; pasMMI < 5; pasMMI++) {
 //   var minMMI = 1;
