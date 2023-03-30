cea8b9548dfc92089cd9bb9dc87081016b36f8df
Guillaume <guillaume.farbos33@gmail.com>
2023-03-30 12:08:20 +0200

    upd: some btn changes

--- public/js/main2.js
+++ public/js/main2.js
@@ -1,6 +1,8 @@
 // script permettant d'acceder à la lecture des tags NFC
 const NFCkey = document.querySelector(".NFCkey");
 let idValue = 1;
+let temps = 8;
+const timerElement = document.getElementById("timer");
 // même script que pour la génération des clés MMI, mais celui ci est pour les clé IUT
 var IUTnums = [8, 9, 10, 11, 12];
 const MAX_KEYS = 3;
@@ -35,11 +37,12 @@ const keyID = document.querySelector(".indiceID"); // constante pour rentrer l'I
 const keyText = document.querySelector(".key__main--text"); // constante pour rentrer la description d'une clé
 const keyImage = document.querySelector(".indiceImage"); // constante pour rentrer l'image' d'une clé
 const keyCount = document.querySelector(".key__footer--text");
+const salleElement = document.querySelector(".key__second__indication");
 // script qui permet de passer à la clé suivante
-
+const keyMain = document.querySelector(".key__mainInfos");
 nextKey.addEventListener("click", () => {
+  keyMain.classList.remove("d-none");
   const keysChildSuivant = keysChilds[indexKey++ % keysChilds.length];
-
   // script qui récupère les données dans le fichier .json et les injecte dans le HTML
   fetch("./js/keys.json")
     .then((response) => response.json())
@@ -47,12 +50,16 @@ nextKey.addEventListener("click", () => {
       if (indexKey > 9) {
         nextKey.disabled = true;
         nextKey.classList.add("grey");
+        treasureSection.classList.remove("d-none");
+        keysSection.classList.add("d-none");
       } else {
         // Les données JSON sont converties objet JavaScript
         const idValue = data.keys[keysChildSuivant.textContent].id;
         // const keyNFC = decoder.decode(record.data);
         const textValue = data.keys[keysChildSuivant.textContent].indiceText;
         const imageValue = data.keys[keysChildSuivant.textContent].image;
+        const salle = data.keys[keysChildSuivant.textContent].salle;
+
         // Les données sont injecter dans le HTML
         keyID.innerText = indexKey;
         keyText.innerText = textValue;
@@ -61,11 +68,25 @@ nextKey.addEventListener("click", () => {
         NFCkey.innerText = idValue;
         console.log(idValue);
         console.log("Index de clé " + indexKey);
+        salleElement.innerText = salle;
         // permet de désactiver le bouton lors de la dernière clé
       }
       nextKey.classList.add("grey");
       nextKey.disabled = true;
       nextKey.innerText = "Clé manquante";
+      setInterval(() => {
+        let minutes = parseInt(temps / 60, 10);
+        let secondes = parseInt(temps % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        secondes = secondes < 10 ? "0" + secondes : secondes;
+
+        timerElement.innerText = `${minutes}:${secondes}`;
+        temps = temps <= 0 ? 0 : temps - 1;
+        if (temps === 0) {
+          salleElement.classList.remove("d-none");
+        }
+      }, 1000);
     });
 });
 
